56dd5527aa79e8451bc44de9cb9116139d6cf3c5
elblasy1986 <117502260+elblasy1986@users.noreply.github.com>
2024-03-12 14:04:18 +0200

    Update data.js

--- sawiris/script/data.js
+++ sawiris/script/data.js
@@ -102,273 +102,265 @@ function getData(){
     boughtItems:0,
     img: "../images/17.jpg"
 },
+{ 
+    item: "كيلو لحمة",
+    price: 360,
+    boughtItems:0,
+    img: "../images/18.jpg"
+},
+{ 
+    item: "زيت ٥ لتر",
+    price: 622,
+    boughtItems:0,
+    img: "../images/19.jpg"
+},
 {
     item: "تفويلة بنزين ٩٥",
     price: 750,
     boughtItems:0,
-    img: "../images/18.jpg"
+    img: "../images/20.jpg"
 },
 {
     item: "تذكرة حفلة لعمرو دياب",
     price: 1750,
     boughtItems:0,
-    img: "../images/19.jpg"
+    img: "../images/21.jpg"
 },
 {
     item: "طقم حلل",
     price: 4200,
     boughtItems:0,
-    img: "../images/20.jpg"
+    img: "../images/22.jpg"
 },
 {
     item: "اشتراك بن سبورت لمدة سنة",
     price: 7650,
     boughtItems:0,
-    img: "../images/21.jpg"
+    img: "../images/23.jpg"
 },
 {
     item: "ليلة فى فندق فاخر",
     price: 10800,
     boughtItems:0,
-    img: "../images/22.jpg"
+    img: "../images/24.jpg"
 },
 {
     item: "اشتراك جولدز جيم لمدة سنة",
     price: 13000,
     boughtItems:0,
-    img: "../images/23.jpg"
+    img: "../images/25.jpg"
 },
 {
     item: "سماعات آير بودز",
     price: 15900,
     boughtItems:0,
-    img: "../images/24.jpg"
+    img: "../images/26.jpg"
 },
 {
     item: "زجاجة برفان أصلية",
     price: 19950,
     boughtItems:0,
-    img: "../images/25.jpg"
-},
-{
-    item: "عجلة ترينكس",
-    price: 32000,
-    boughtItems:0,
-    img: "../images/26.jpg"
-},
-{
-    item: "موتوسيكل حلاوة صينى",
-    price: 39000,
-    boughtItems:0,
     img: "../images/27.jpg"
-},
-{
-    item: "بلايستيشن ٥",
-    price: 40500,
-    boughtItems:0,
-    img: "../images/28.jpg"
-},
-{
-    item: "سامسونج جالاكسى إس٢٤",
-    price: 24000,
-    boughtItems:0,
-    img: "../images/29.jpg"
 },
 {
     item: "بدلة",
     price: 24200,
     boughtItems:0,
-    img: "../images/30.jpg"
-},
-{
-    item: "عملية تجميل",
-    price: 25000,
-    boughtItems:0,
-    img: "../images/31.jpg"
+    img: "../images/28.jpg"
 },
 {
     item: "فستان",
     price: 29700,
     boughtItems:0,
+    img: "../images/29.jpg"
+},
+{
+    item: "عجلة ترينكس",
+    price: 32000,
+    boughtItems:0,
+    img: "../images/30.jpg"
+},
+{ 
+    item: "غسالة ملابس",
+    price: 33100,
+    boughtItems:0,
+    img: "../images/31.jpg"
+},
+{
+    item: "موتوسيكل حلاوة صينى",
+    price: 39000,
+    boughtItems:0,
     img: "../images/32.jpg"
 },
 {
-    item: "رحلة عمرة",
-    price: 35000,
+    item: "بلايستيشن ٥",
+    price: 40500,
     boughtItems:0,
     img: "../images/33.jpg"
 },
 {
-    item: "آيفون ١٥ برو",
-    price: 41800,
+    item: "عملية تجميل",
+    price: 50000,
     boughtItems:0,
     img: "../images/34.jpg"
 },
 {
-    item: "تليفيزيون ٦٥ بوصة",
-    price: 48000,
+    item: "سامسونج جالاكسى إس٢٤",
+    price: 65700,
     boughtItems:0,
     img: "../images/35.jpg"
 },
 {
-    item: "ماك بوك برو",
-    price: 52500,
+    item: "رحلة عمرة",
+    price: 70000,
     boughtItems:0,
     img: "../images/36.jpg"
 },
 {
-    item: "سبيكة ذهب ٥٠ جرام",
-    price: 52800,
+    item: "آيفون ١٥ برو",
+    price: 51900,
     boughtItems:0,
     img: "../images/37.jpg"
 },
 {
-    item: "توكتوك",
-    price: 60000,
+    item: "تليفيزيون ٦٥ بوصة",
+    price: 55000,
     boughtItems:0,
     img: "../images/38.jpg"
 },
 {
-    item: "رحلة حج",
-    price: 92000,
+    item: "توكتوك",
+    price: 91000,
     boughtItems:0,
     img: "../images/39.jpg"
 },
 {
-    item: "كمبيوتر ألعاب متطور",
-    price: 120000,
+    item: "ماك بوك برو",
+    price: 172000,
     boughtItems:0,
     img: "../images/40.jpg"
 },
 {
-    item: "ساعة رولكس",
-    price: 130000,
+    item: "سبيكة ذهب ٥٠ جرام",
+    price: 199600,
     boughtItems:0,
     img: "../images/41.jpg"
 },
 {
-    item: "موتوسيكل هوندا",
-    price:180000 ,
+    item: "كمبيوتر ألعاب متطور",
+    price: 242000,
     boughtItems:0,
     img: "../images/42.jpg"
 },
 {
-    item: "فدان أرض زراعية",
-    price: 250000,
+    item: "رحلة حج",
+    price: 450000,
     boughtItems:0,
     img: "../images/43.jpg"
 },
 {
-    item: "نيسان صنى",
-    price: 297000,
+    item: "ساعة رولكس",
+    price: 482000,
     boughtItems:0,
     img: "../images/44.jpg"
 },
 {
-    item: "خاتم ألماظ",
-    price: 300000,
+    item: "موتوسيكل هوندا",
+    price:586000 ,
     boughtItems:0,
     img: "../images/45.jpg"
 },
 {
-    item: "جيت سكى ياماها",
-    price: 370000,
+    item: "خاتم ألماظ",
+    price: 612000,
     boughtItems:0,
     img: "../images/46.jpg"
 },
-
 {
-    item: "تويوتا كرولا",
-    price: 420000,
+    item: "نيسان صنى",
+    price: 761000,
     boughtItems:0,
     img: "../images/47.jpg"
 },
-
 {
-    item: "تيسلا إكس",
-    price: 1170000,
+    item: "جيت سكى ياماها",
+    price: 864000,
     boughtItems:0,
     img: "../images/48.jpg"
 },
-
 {
-    item: "بى إم دبليو إكس ٦",
-    price: 2500000,
+    item: "تويوتا كرولا",
+    price: 1800000,
     boughtItems:0,
     img: "../images/49.jpg"
 },
-
 {
-    item: "فيلا فى التجمع",
-    price: 4300000,
+    item: "فدان أرض زراعية",
+    price: 2300000,
     boughtItems:0,
     img: "../images/50.jpg"
 },
 {
-    item: "شقة على النيل",
-    price: 7500000,
+    item: "تيسلا إكس",
+    price: 4400000,
     boughtItems:0,
     img: "../images/51.jpg"
 },
 {
-    item: "فيلا فى الساحل",
-    price: 8000000,
+    item: "بى إم دبليو إكس ٦",
+    price: 8700000,
     boughtItems:0,
     img: "../images/52.jpg"
 },
+
 {
-    item: "إنتاج فيلم سينمائى",
-    price: 20000000,
+    item: "فيلا فى التجمع",
+    price: 9200000,
     boughtItems:0,
     img: "../images/53.jpg"
 },
 {
-    item: "رحلة أسبوع فى كل دولة",
-    price: 25000000,
+    item: "شقة على النيل",
+    price: 15200000,
     boughtItems:0,
     img: "../images/54.jpg"
 },
 {
-    item: "يخت",
-    price: 150000000,
+    item: "فيلا فى الساحل",
+    price: 16000000,
     boughtItems:0,
     img: "../images/55.jpg"
 },
 {
-    item: "دبابة",
-    price: 160000000,
+    item: "إنتاج فيلم سينمائى",
+    price: 40000000,
     boughtItems:0,
     img: "../images/56.jpg"
 },
-{ 
-    item: "هليكوبتر أباتشى",
-    price: 620000000,
+{
+    item: "رحلة أسبوع فى كل دولة",
+    price: 50000000,
     boughtItems:0,
     img: "../images/57.jpg"
 },
-{ 
-    item: "هليكوبتر أباتشى",
-    price: 620000000,
+{
+    item: "يخت",
+    price: 300000000,
     boughtItems:0,
     img: "../images/58.jpg"
 },
-{ 
-    item: "هليكوبتر أباتشى",
-    price: 620000000,
+{
+    item: "دبابة",
+    price: 310000000,
     boughtItems:0,
     img: "../images/59.jpg"
 },
 { 
     item: "هليكوبتر أباتشى",
-    price: 620000000,
+    price: 1240000000,
     boughtItems:0,
     img: "../images/60.jpg"
 },
-
-
-
-
-
 
 ]
 return items;
